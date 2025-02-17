1af201f3d6a3fee421e72133ca4970db6e86f765
dbarkanov <technick303@gmail.com>
2025-02-17 15:33:29 +0300

    delete modules main

--- modules/vpc/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "subnet-1" {
-  value = yandex_vpc_subnet.subnet-1.id
-}
-
-output "vpc_id" {
-  value = yandex_vpc_network.network-1.id
-}
--- modules/vpc/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "cidr_block" {
-  description = "CIDR bloks of IPv4 subnts"
-  type        = list(any)
-  default     = ["192.168.1.0/24", "192.168.10.0/24"]
-}
